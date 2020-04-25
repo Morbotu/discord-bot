7938bc3e8c1e995991841bf4d1347288f0e197dd
Rombout Jansen <romboutjansen@MacBook-Pro-van-Walter.local>
2020-04-25 16:40:43 +0200

    add sayHello function

--- events/message.js
+++ events/message.js
@@ -1,6 +1,10 @@
-const kick = require("../commands/kick")
+const kick = require("../commands/kick");
+const sayHello = require("../commands/sayHello");
 module.exports = (client, message) => {
     if (message.content.startsWith("!kick")) {
-        return kick(message)
+        return kick(message);
+    }
+    if (lower(message.content) == "hello") {
+        return sayHello(message);
     }
 }
