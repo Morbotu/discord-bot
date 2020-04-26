bc8bcb3a7f9543bd2437729b612b55a977da96dd
Rombout Jansen <romboutjansen@MacBook-Pro-van-Walter.local>
2020-04-26 09:31:09 +0200

    small test

--- commands/duel.js
+++ commands/duel.js
@@ -3,7 +3,7 @@ module.exports = async (keyv, message) => {
     const channel = message.channel;
     if (!opponent) return message.reply(`Who are you trying to duel? You must mention a user.`);
     else if (!(await keyv.get("duel " + message.author.id))) {
-        await keyv.set("duel " + opponent.id, true);
+        await keyv.set("duel " + message.author.id, true);
         return channel.send("<@" + opponent.user.id + ">" + " has 30 seconds to reply");
     }
     else keyv.clear();
