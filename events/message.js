dfe4b08146e8cf51a4b7217252ffa4470894f050
Rombout Jansen <romboutjansen@MacBook-Pro-van-Walter.local>
2020-04-28 14:08:57 +0200

    delete kick.js

--- events/message.js
+++ events/message.js
@@ -1,11 +1,8 @@
-const kick = require("../commands/kick");
 const sayHello = require("../commands/sayHello");
 const duel = require("../commands/duel");
 const getActiveRooms = require("../commands/getActiveRooms");
 module.exports = (client, MessageEmbed, keyv, message) => {
     if (message.author.bot) return; // Doesn't awnser to bots.
-    if (message.content.startsWith("!kick"))
-        return kick(message);
     if (["hello","hallo","goedendag"].includes(message.content.toLowerCase()))
         return sayHello(message);
     if (["duel","yes","no","bite","punch","stab","mega_punch"].includes(message.content.toLowerCase().split(" ")[0]))
