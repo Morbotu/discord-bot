a11802d507878b08912079e3ad3e0a8bdbefde95
Rombout Jansen <romboutjansen@MacBook-Pro-van-Walter.local>
2020-05-01 15:53:26 +0200

    some improvement to help.js

--- commands/help.js
+++ commands/help.js
@@ -4,13 +4,13 @@ module.exports = (MessageEmbed, message) => {
         if (helpSubject === "duel")
             return message.channel.send(new MessageEmbed()
             .setTitle("Help page: duel")
-            .setDescription("Usage:\n`R!duel @player`\nDescription:\nChallenge other players in a luck based duel.")
+            .setDescription("**Usage:**\n`R!duel @player`\n\n**Description:**\nChallenge other players in a luck based duel.")
             .setColor(0xff0000)
         );
     }
     return message.channel.send(new MessageEmbed()
         .setTitle("Help page")
-        .setDescription("Commands: \n`duel`")
+        .setDescription("**Commands:**\n`duel`")
         .setColor(0xff0000)
     );
 }
