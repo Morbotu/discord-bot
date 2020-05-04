055083713279a6520fbaf912ba62d9b5a08622b4
Rombout Jansen <romboutjansen@MacBook-Pro-van-Walter.local>
2020-05-04 10:44:42 +0200

    new branch

--- commands/button.js
+++ commands/button.js
@@ -1,4 +1,6 @@
 module.exports = message => {
+    if (message.content.split().lenght > 2)
+        return message.reply("This command only has one argument.");
     const colors = ["blue", "red"];
     for (const color in colors)
         if (!(message.guild.roles.cache.find(role => role.name === `${colors[color]} tier`)))
