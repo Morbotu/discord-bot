39e645149f8efa793f2057163193d3c9fdb2c740
Rombout Jansen <romboutjansen@MacBook-Pro-van-Walter.local>
2020-05-04 22:10:38 +0200

    more update to button.js

--- commands/button.js
+++ commands/button.js
@@ -10,12 +10,9 @@ module.exports = async (message, keyv, MessageEmbed) => {
         for (const color of colors)
             check.push(`${color} tier`);
         const roles = guild.roles.cache.filter(role => !(check.includes(role.name)));
-        console.log(roles);
         for (const [id, name] of roles) {
-            console.log(id);
-            console.log(guild.roles.cache.find(role => role.id === id))
-            guild.roles.cache.find(role => role.id === id).setColor("RED");
-            // console.log(guild.roles.change.find(role => role.id === id));
+            if (!(name.name === "@everyone") && !(name.name === "Rombot"))
+                guild.roles.cache.find(role => role.id === id).setColor("STANDARD");
         }
         for (const color of colors) {
             if (!(guild.roles.cache.find(role => role.name === `${color} tier`)))
@@ -28,29 +25,44 @@ module.exports = async (message, keyv, MessageEmbed) => {
                     reason: `button ${color} tier`,
                 });
         }
-        await keyv.set(guild.id + ":buttonAlive", true)
         await keyv.set(guild.id + ":button", colors.length);
-        setInterval(async function() {
-            if (await keyv.get(guild.id + ":buttonAlive")) {
-                await keyv.set(guild.id + ":button", await keyv.get(guild.id + ":button") - 1);
-                if (await keyv.get(guild.id + ":button") === 0) {
-                    await keyv.delete(guild.id + ":buttonAlive");
-                    return channel.send(new MessageEmbed()
-                        .setTitle("Button died")
-                        .setDescription("Type `R!button start` to start the button again.")
-                        .setColor(0xff0000)
-                    );
-                }
+        let interval = setInterval(async function() {
+            await keyv.set(guild.id + ":button", await keyv.get(guild.id + ":button") - 1);
+            if (await keyv.get(guild.id + ":button") === 0) {
+                clearInterval(interval);
+                return channel.send(new MessageEmbed()
+                    .setTitle("Button died")
+                    .setDescription("Type `R!button start` to start the button again.")
+                    .setColor(0xff0000)
+                );
             }
         }, 10000);
     }
     if (arg === "press") {
-
+        for (const color of colors) {
+            message.member.roles.remove(guild.roles.cache.find(role => role.name === `${color} tier`));
+        }
+        let roleColor = await keyv.get(guild.id + ":button")-1;
+        message.member.roles.add(guild.roles.cache.find(role => role.name === `${colors[roleColor]} tier`));
     }
-    if (arg === "look")
+    if (arg === "look") {
+        let currColorIndex = await keyv.get(guild.id + ":button")-1;
+        let currColor = colors[currColorIndex];
+        if (!currColor)
+            return message.reply("The button  is dead or not installed yet.");
+        let colorEmojis = "";
+        let countdownEmoji = "";
+        for (const color of colors)
+            colorEmojis = colorEmojis + `:${color}_square:`;
+        for (let i = 0; i < colors.length; i++)
+            if (i < currColorIndex+1)
+                countdownEmoji = countdownEmoji + ":white_large_square:";
+            else
+                countdownEmoji = countdownEmoji + ":white_square_button:";
         return channel.send(new MessageEmbed()
             .setTitle("Button")
-            .setDescription(`The button is now add ${colors[await keyv.get(guild.id + ":button")-1]}`)
+            .setDescription(`The button is now ${currColor}\n${colorEmojis}\n${countdownEmoji}`)
             .setColor(0xff0000)
         );
+    }
 }
