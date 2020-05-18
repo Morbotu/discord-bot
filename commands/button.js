1bb8b5d7404fec436d3a4f8eb8c6c2e15c52af71
Rombout Jansen <romboutlambert@gmail.com>
2020-05-18 12:02:40 +0200

    make reset go faster

--- commands/button.js
+++ commands/button.js
@@ -95,10 +95,12 @@ module.exports = async (message, keyv, MessageEmbed) => {
 
     if (arg === "reset") {
         for (const guildMember of guild.members.cache)
-            for (const color of buttonColors)
-                guildMember[1].roles.remove(
-                    guild.roles.cache.find((role) => role.name === `${color} tier`)
-                );
+            if (!guildMember[1].user.bot)
+                for (const color of buttonColors)
+                    if (guildMember[1].roles.cache.find((role) => role.name === `${color} tier`))
+                        guildMember[1].roles.remove(
+                            guild.roles.cache.find((role) => role.name === `${color} tier`)
+                        );
         return message.reply("Reset all tiers.");
     }
 
