c52471e7497831376b97ba4c0f5e6fbc1686cd9f
Rombout Jansen <romboutlambert@gmail.com>
2020-06-11 10:13:54 +0200

    fixed a bug with await

--- commands/rombotFace.js
+++ commands/rombotFace.js
@@ -30,12 +30,12 @@ function imageToMessage(readimage, fs, filedata, frame) {
 
 module.exports = async (message) => {
     var channel = message.channel;
-    var faceMessageInstance = channel.send(imageToMessage(readimage, fs, filedata, 0));
+    var faceMessageInstance = await channel.send(imageToMessage(readimage, fs, filedata, 0));
     var frame = 1;
-    var interval = setInterval(() => {
+    var interval = setInterval(async () => {
         let text = imageToMessage(readimage, fs, filedata, frame);
         if (text === "done") return clearInterval(interval);
-        faceMessageInstance.edit(text);
+        await faceMessageInstance.edit(text);
         frame++;
     }, 1000);
 };
