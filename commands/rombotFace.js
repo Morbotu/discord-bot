25a12d370a42b43024307340dbcae581a6655e34
Rombout Jansen <romboutlambert@gmail.com>
2020-06-11 10:08:45 +0200

    animation working

--- commands/rombotFace.js
+++ commands/rombotFace.js
@@ -1,31 +1,41 @@
 const readimage = require("readimage");
 const fs = require("fs");
-const filedata = fs.readFileSync("./animations/face.jpg");
+const filedata = fs.readFileSync("./animations/animation.gif");
 
-function imageToMessage(readimage, fs, filedata) {
-    var faceMessage = "";
+function imageToMessage(readimage, fs, filedata, frame) {
+    var gif;
     readimage(filedata, function (err, image) {
         if (err) {
             console.log("failed to read the image");
             console.log(err);
         }
-        for (let i = 0; i < image.height; i++) {
-            for (let j = 0; j < image.width * 4; j += 4) {
-                let r = image.frames[0].data[j + i * image.width * 4];
-                let g = image.frames[0].data[j + i * image.width * 4 + 1];
-                let b = image.frames[0].data[j + i * image.width * 4 + 2];
-                if (r === 0 && g === 0 && b === 0)
-                    faceMessage = faceMessage.concat(":black_large_square:");
-                if (r === 255 && g === 255 && b === 0)
-                    faceMessage = faceMessage.concat(":yellow_square:");
-            }
-            faceMessage = faceMessage.concat("\n");
-        }
+        gif = image;
     });
+    if (frame === gif.frames.length) return "done";
+    var faceMessage = "";
+    for (let i = 0; i < gif.height; i++) {
+        for (let j = 0; j < gif.width * 4; j += 4) {
+            let r = gif.frames[frame].data[j + i * gif.width * 4];
+            let g = gif.frames[frame].data[j + i * gif.width * 4 + 1];
+            let b = gif.frames[frame].data[j + i * gif.width * 4 + 2];
+            if (r === 0 && g === 0 && b === 0)
+                faceMessage = faceMessage.concat(":black_large_square:");
+            if (r === 255 && g === 255 && b === 0)
+                faceMessage = faceMessage.concat(":yellow_square:");
+        }
+        faceMessage = faceMessage.concat("\n");
+    }
     return faceMessage;
 }
 
 module.exports = async (message) => {
     var channel = message.channel;
-    channel.send(imageToMessage(readimage, fs, filedata));
+    var faceMessageInstance = channel.send(imageToMessage(readimage, fs, filedata, 0));
+    var frame = 1;
+    var interval = setInterval(() => {
+        let text = imageToMessage(readimage, fs, filedata, frame);
+        if (text === "done") return clearInterval(interval);
+        faceMessageInstance.edit(text);
+        frame++;
+    }, 1000);
 };
