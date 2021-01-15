fe8aa4cb7c39597a567954733443a30ae3d49ae0
Morbotu <romboutlambert@gmail.com>
2021-01-15 15:56:05 +0100

    Say hello in voice chat

--- commands/sayHello.js
+++ commands/sayHello.js
@@ -1,4 +1,11 @@
 module.exports = (message) => {
     const channel = message.channel;
     channel.send(message.content + " <@" + message.author.id + ">");
+    if (message.member.voice.channel) {
+        message.member.voice.channel.join().then((connection) => {
+            connection.play("./sounds/helloSound.mp3").on("finish", () => {
+                connection.disconnect();
+            });
+        });
+    };
 };
