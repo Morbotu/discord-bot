a08d2b283242532c297ce012740f69b93fa2b5dc
Morbotu <romboutlambert@gmail.com>
2021-01-18 15:15:20 +0100

    Add delay before saying hello

--- events/voiceStateUpdate.js
+++ events/voiceStateUpdate.js
@@ -2,11 +2,13 @@ module.exports = (oldState, newState) => {
     if (oldState.member.user.bot) return;
 
     if (newState.channel && !newState.selfDeaf && oldState.selfDeaf || !oldState.channel && newState.channel && !newState.selfDeaf) {
-        newState.channel.join().then((connection) => {
-            connection.play("./sounds/helloSound.mp3").on("finish", () => {
-                connection.disconnect();
+        setTimeout(() => {
+            newState.channel.join().then((connection) => {
+                connection.play("./sounds/helloSound.mp3").on("finish", () => {
+                    connection.disconnect();
+                });
             });
-        });
+        }, 1500);
     };
 
     if (oldState.channel && !newState.channel || newState.selfDeaf && oldState.channel) {
