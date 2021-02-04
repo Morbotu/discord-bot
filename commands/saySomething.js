05795d37b5ddf7651065f008a5532eeabb87da9c
Morbotu <romboutlambert@gmail.com>
2021-02-04 16:25:12 +0100

    Make able to say line by line

--- commands/saySomething.js
+++ commands/saySomething.js
@@ -1,11 +1,18 @@
-const discordTTS=require("discord-tts");
+const discordTTS = require("discord-tts");
+
+function sayTextRecursively(text, connection) {
+    let speakText = text.splice(0, 1).join(" ").substring(0, 200);
+    connection.play(discordTTS.getVoiceStream(speakText)).on("finish", () => {
+        if (text.length == 0)
+            connection.disconnect();
+        else sayTextRecursively(text, connection)
+    });
+}
 
 module.exports = (message) => {
     if (message.member.voice.channel) {
         message.member.voice.channel.join().then((connection) => {
-            connection.play(discordTTS.getVoiceStream(message.content.toLowerCase().replace("r!say ", "").substring(0, 200))).on("finish", () => {
-                connection.disconnect();
-            });
+            sayTextRecursively(message.content.toLowerCase().replace("r!say ", "").split(/[\n,.]/).filter((item) => item !== ""), connection);
         }).catch(console.error);
     };
 }
