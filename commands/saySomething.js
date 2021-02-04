65b5ab6def138b8d99644f5d6cf1827e35d087e6
Morbotu <romboutlambert@gmail.com>
2021-02-04 10:22:28 +0100

    Cut message to 200 characters

--- commands/saySomething.js
+++ commands/saySomething.js
@@ -3,7 +3,7 @@ const discordTTS=require("discord-tts");
 module.exports = (message) => {
     if (message.member.voice.channel) {
         message.member.voice.channel.join().then((connection) => {
-            connection.play(discordTTS.getVoiceStream(message.content.toLowerCase().replace("r!say ", ""))).on("finish", () => {
+            connection.play(discordTTS.getVoiceStream(message.content.toLowerCase().replace("r!say ", "").substring(0, 200))).on("finish", () => {
                 connection.disconnect();
             });
         }).catch(console.error);
